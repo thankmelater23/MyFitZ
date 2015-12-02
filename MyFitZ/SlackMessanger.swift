7593a7de94a39f829fb0ddfa9bc0eaf2b9df7655
Andre Villanueva <thankmelater23@me.com>
2015-12-02 02:56:35 -0800

    Pods added

---
+++ MyFitZ/SlackMessanger.swift
@@ -0,0 +1,13 @@
+//
+//  SlackMessanger.swift
+//  MyFitZ
+//
+//  Created by Andre Villanueva on 12/1/15.
+//  Copyright © 2015 Bang Bang Studios. All rights reserved.
+//
+
+import Cocoa
+
+class SlackMessanger: UITableViewController {
+
+}
